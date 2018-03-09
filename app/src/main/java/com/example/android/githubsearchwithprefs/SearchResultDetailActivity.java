903a97eeb8c1f1b886d752e5d186c77ca15de7b2
Austin Kwong <16788439+kwongar@users.noreply.github.com>
2018-03-08 20:34:03 -0800

    First commit from week 7 prefs
    Sorry I haven't Made any real changes.

---
+++ app/src/main/java/com/example/android/githubsearchwithprefs/SearchResultDetailActivity.java
@@ -0,0 +1,81 @@
+package com.example.android.githubsearchwithprefs;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.android.githubsearchwithprefs.utils.GitHubUtils;
+
+public class SearchResultDetailActivity extends AppCompatActivity {
+
+    private TextView mTVSearchResultName;
+    private TextView mTVSearchResultStars;
+    private TextView mTVSearchResultDescription;
+
+    private GitHubUtils.SearchResult mSearchResult;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result_detail);
+
+        mTVSearchResultName = (TextView)findViewById(R.id.tv_search_result_name);
+        mTVSearchResultStars = (TextView)findViewById(R.id.tv_search_result_stars);
+        mTVSearchResultDescription = (TextView)findViewById(R.id.tv_search_result_description);
+
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra(GitHubUtils.EXTRA_SEARCH_RESULT)) {
+            mSearchResult = (GitHubUtils.SearchResult) intent.getSerializableExtra(GitHubUtils.EXTRA_SEARCH_RESULT);
+            mTVSearchResultName.setText(mSearchResult.fullName);
+            mTVSearchResultStars.setText(String.valueOf(mSearchResult.stars));
+            mTVSearchResultDescription.setText(mSearchResult.description);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_result_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_view_repo:
+                viewRepoOnWeb();
+                return true;
+            case R.id.action_share:
+                shareRepo();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void viewRepoOnWeb() {
+        if (mSearchResult != null) {
+            Uri githubRepoURL = Uri.parse(mSearchResult.htmlURL);
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, githubRepoURL);
+            if (webIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(webIntent);
+            }
+        }
+    }
+
+    public void shareRepo() {
+        if (mSearchResult != null) {
+            String shareText = getString(R.string.share_text_prefix) + ": " +
+                  mSearchResult.fullName + ", " + mSearchResult.htmlURL;
+
+            ShareCompat.IntentBuilder.from(this)
+                    .setChooserTitle(R.string.share_chooser_title)
+                    .setType("text/plain")
+                    .setText(shareText)
+                    .startChooser();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/githubsearchwithprefs/utils/GitHubUtils.java
@@ -0,0 +1,89 @@
+package com.example.android.githubsearchwithprefs.utils;
+
+import android.net.Uri;
+import android.text.TextUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by hessro on 1/30/18.
+ */
+
+public class GitHubUtils {
+    public static final String EXTRA_SEARCH_RESULT = "GitHubUtils.SearchResult";
+
+    final static String GITHUB_SEARCH_BASE_URL = "https://api.github.com/search/repositories";
+    final static String GITHUB_SEARCH_QUERY_PARAM = "q";
+    final static String GITHUB_SEARCH_SORT_PARAM = "sort";
+
+    public static class SearchResult implements Serializable {
+        public String fullName;
+        public String description;
+        public String htmlURL;
+        public int stars;
+    }
+
+    public static String buildGitHubSearchURL(String searchQuery, String sort, String language,
+                                              String user, boolean searchInName, boolean searchInDescription,
+                                              boolean searchInReadme) {
+
+        Uri.Builder builder = Uri.parse(GITHUB_SEARCH_BASE_URL).buildUpon();
+
+        if (!TextUtils.isEmpty(sort)) {
+                builder.appendQueryParameter(GITHUB_SEARCH_SORT_PARAM, sort);
+        }
+
+        String queryValue = new String(searchQuery);
+        if (!TextUtils.isEmpty(language)) {
+            queryValue += " language:" + language;
+        }
+        if (!TextUtils.isEmpty(user)) {
+            queryValue += " user:" + user;
+        }
+
+        ArrayList<String> searchInList = new ArrayList<>();
+        if (searchInName) {
+            searchInList.add("name");
+        }
+        if (searchInDescription) {
+            searchInList.add("description");
+        }
+        if (searchInReadme) {
+            searchInList.add("readme");
+        }
+        if (!searchInList.isEmpty()) {
+            queryValue += " in:" + TextUtils.join(",", searchInList);
+        }
+
+        builder.appendQueryParameter(GITHUB_SEARCH_QUERY_PARAM, queryValue);
+
+        return builder.build().toString();
+    }
+
+    public static ArrayList<SearchResult> parseSearchResultsJSON(String searchResultsJSON) {
+        try {
+            JSONObject searchResultsObj = new JSONObject(searchResultsJSON);
+            JSONArray searchResultsItems = searchResultsObj.getJSONArray("items");
+
+            ArrayList<SearchResult> searchResultsList = new ArrayList<SearchResult>();
+            for (int i = 0; i < searchResultsItems.length(); i++) {
+                SearchResult result = new SearchResult();
+                JSONObject resultItem = searchResultsItems.getJSONObject(i);
+                result.fullName = resultItem.getString("full_name");
+                result.description = resultItem.getString("description");
+                result.htmlURL = resultItem.getString("html_url");
+                result.stars = resultItem.getInt("stargazers_count");
+                searchResultsList.add(result);
+            }
+            return searchResultsList;
+        } catch (JSONException e) {
+            return null;
+        }
+    }
+}
